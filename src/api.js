4e725cc5ac958a55fa00e4d0541ac913b0698e33
Nadeem1221 <nadeem562003@gmail.com>
2026-03-15 03:25:46 +0530

    deloying the project

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://ai-assisted-journal-system-dp4w.onrender.com';
+// const API_BASE = 'http://localhost:5000/api';
 
 export const createEntry = (data) =>
   axios.post(`${API_BASE}/journal`, data).then((r) => r.data);
